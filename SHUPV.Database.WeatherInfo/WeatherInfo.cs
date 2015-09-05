a74541e545b6f911aae7920a3400d2142942b8b9
wolfogre <wolfogre@qq.com>
2015-09-05 12:23:02 +0800

    移除提供数据账户的库工程

---
+++ SHUPV.Database.WeatherInfo/WeatherInfo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+using SHUPV.Database;
+
+namespace SHUPV.Database
+{
+	public class WeatherInfo
+	{
+
+		public bool IsEmpty { get { return _isEmpty; } }
+
+		public WeatherInfo(double latitude, double longitude)
+		{
+			_lat = latitude;
+			_lon = longitude;
+			_sqlCon = WeatherDatabase.GetSqlConnection();
+		}
+
+		public void Open()
+		{
+			try
+			{
+				_sqlCon.Open();
+			}
+			catch(Exception ex)
+			{
+				throw ex;
+			}
+		}
+
+		double _lat;
+		double _lon;
+		SqlConnection _sqlCon;
+		bool _isEmpty;
+	}
+}
