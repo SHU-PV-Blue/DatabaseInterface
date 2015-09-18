81d310ec111f882fb5bb56d0a11b2bd61bb98824
wolfogre <wolfogre@qq.com>
2015-09-18 23:00:10 +0800

    测试GetTableData()成功

--- Test_SHUPV.Database/Program.cs
+++ Test_SHUPV.Database/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SHUPV.Database;
+using System.Data;
 
 namespace Test_SHUPV.Database
 {
@@ -11,11 +12,34 @@ namespace Test_SHUPV.Database
 	{
 		static void Main(string[] args)
 		{
+			TestGetTableData();
+		}
+		
+		static void TestGetPartNames()
+		{
 			WeatherData wd = new WeatherData();
 			Console.WriteLine("获得表名：");
 			List<string> ls = wd.GetPartNames();
 			foreach (string str in ls)
 				Console.WriteLine(str);
+		}
+
+		static void TestGetTableData()
+		{
+			WeatherData wd = new WeatherData();
+			Console.WriteLine("获得数据：");
+			DataTable dt = wd.GetTableData(10,10,
+				"Parameters for Sizing Battery or other Energy-storage Systems",
+				"Equivalent Number Of NO-SUN Or BLACK Days");
+			dt.Columns.Remove("LineID");
+			dt.Columns.Remove("TableID");
+			foreach (DataRow dr in dt.Rows)
+			{
+				for(int i = 0; i < dt.Columns.Count; ++i)
+					Console.Write(dr[i] + " ");
+				Console.WriteLine();
+			}
+				
 		}
 	}
 
